1b7a2651434e883bd1f2a8507becf9148bb42126
Thomas <thomas.bartels@nos.nl>
2018-04-20 22:56:37 +0100

    Temp revert for breaking uploader

--- app/laravel/app/Http/Controllers/Uploader.php
+++ app/laravel/app/Http/Controllers/Uploader.php
@@ -4,6 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+
+class Uploader extends Base
+{
+    public function upload(Request $request)
+    {
+        $folder = DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR;
+        $path = '..' . DIRECTORY_SEPARATOR . 'schijf-api';
+        $image = Input::file('image');
+
+        if ($image) {
+            $fileName = uniqid('upload_') . '_' . strtolower($image->getClientOriginalName());
+
+            $image->move($path . $folder, $fileName);
+
+            return response()->json([
+                'url' => $folder . $fileName
+            ], 201);
+        }
+    }
+}
+
+/*
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use Intervention\Image\Facades\Image;
 
 class Uploader extends Base
@@ -29,3 +55,4 @@ class Uploader extends Base
         }
     }
 }
+*/
